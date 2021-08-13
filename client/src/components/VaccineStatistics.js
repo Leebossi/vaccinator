88cfc300335a7e419bb1c0512f61e4f73be506aa
Leevi Ossi <leevi.ossi@hotmail.com>
2021-08-13 11:18:02 +0300

    VaccineStatistics shows amount of injections in arrived orders

--- client/src/components/VaccineStatistics.js
+++ client/src/components/VaccineStatistics.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 const VaccineStatistics = ({vaccine, brand}) => {
+  const getInjections = (vaccine) => {
+    let injections = 0
+    for (let i = 0; i < vaccine.length; i++) {
+      injections += vaccine[i].injections  
+    }
+    return injections
+  }
+
   if (vaccine.length <= 0) {
     return null
   }
@@ -9,6 +17,7 @@ const VaccineStatistics = ({vaccine, brand}) => {
     <div>
       <h2>{brand}</h2>
       <p>orders arrived: {vaccine.length}</p>
+      <p>amount of injections in arrived orders: {getInjections(vaccine)}</p>
     </div>
   )
 }
