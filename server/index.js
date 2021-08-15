be03223444ee758566268f00ca21c73b87592a03
Leevi Ossi <leevi.ossi@hotmail.com>
2021-08-15 15:04:19 +0300

    backend heroku ready

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.get('/api/vaccinations', (req, res) => {
   res.json(vaccinations)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
 })
